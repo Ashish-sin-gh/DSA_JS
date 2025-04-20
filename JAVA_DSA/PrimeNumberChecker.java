0d8d3c2598cca6b374b9fa7506b4dadedd009884
ashish kumar singh <ashish159singh@gmail.com>
2025-04-20 19:52:52 +0530

    dsa java basics

---
+++ JAVA_DSA/PrimeNumberChecker.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class PrimeNumberChecker{
+
+    public static String primeCheck(int num){
+        if (num == 0 || num == 1){
+            return "not prime";
+        }
+        for(int i = 2; i<=num/2; i++){
+            if(num%i == 0 ){
+                return "not prime";
+            } 
+        }
+        return "is prime";
+    }
+
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("enter the number:");
+        int num = scn.nextInt();
+
+        
+
+        System.out.println(primeCheck(num));
+    }
+}
---
+++ JAVA_DSA/basics.java
@@ -0,0 +1,12 @@
+import  java.util.*;
+
+class Basics{
+    public static void main(String [] args){
+        System.out.println("ashish hello");
+
+        Scanner scn = new Scanner(System.in);
+        String s = scn.nextLine();
+        int i = Integer.parseInt(scn.nextLine());
+        System.out.println(s + " is " + i + " years old");
+    }
+}
