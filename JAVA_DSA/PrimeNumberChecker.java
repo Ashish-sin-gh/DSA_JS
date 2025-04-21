b56c516da6473dfe26c6c58c4f4af60907e555a1
ashish kumar singh <ashish159singh@gmail.com>
2025-04-21 15:44:52 +0530

    PrintAllPrime

--- JAVA_DSA/PrimeNumberChecker.java
+++ JAVA_DSA/PrimeNumberChecker.java
@@ -1,3 +1,10 @@
+// any number that want to get divided will get divided from a number btn 1 and 1quare root of that number or else it wont get divided.
+/*
+    if p*q = n
+    then p < root n && q < root n 
+    (i.e. both p and q cant be greater than root n) 
+    (atleast one will be less than root n)
+' */
 import java.util.*;
 
 class PrimeNumberChecker{
@@ -6,7 +13,7 @@ class PrimeNumberChecker{
         if (num == 0 || num == 1){
             return "not prime";
         }
-        for(int i = 2; i<=num/2; i++){
+        for(int i = 2; i*i <= num; i++){
             if(num%i == 0 ){
                 return "not prime";
             } 
---
+++ JAVA_DSA/PrintAllPrimes.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class PrintAllPrimes{
+    public static void printPrime(int low,int high){
+        for(int i = low; i <= high; i++){
+            boolean isPrime = true;
+            for(int j = 2; j * j <= i; j++){
+                if(i % j == 0){
+                    isPrime = false;
+                    break;
+                } 
+            }
+            if(isPrime){
+                System.out.println(i);
+            }
+        }
+    }
+    
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int low = scn.nextInt();
+        int high = scn.nextInt();
+
+        printPrime(low, high);
+    }
+}
