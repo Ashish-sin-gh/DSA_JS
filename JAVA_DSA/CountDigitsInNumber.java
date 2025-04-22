f542b06b7c335dc0055b15493808d157983ae9e4
ashish kumar singh <ashish159singh@gmail.com>
2025-04-22 16:17:01 +0530

    count digit in a number program

---
+++ JAVA_DSA/CountDigitsInNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class CountDigitsInNumber{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int count = 0;
+        System.out.print("enter the number:");
+        int div = scn.nextInt();
+
+        while(div > 0){
+            count++;
+            div = div/10;
+        }
+        System.out.println("number of digits are: " + count);
+    }
+}
