e5ca8623ad01dcea342857f3b1512a79d85d97b9
ashish kumar singh <ashish159singh@gmail.com>
2025-04-24 10:57:24 +0530

    rotation of number added

---
+++ JAVA_DSA/RotateNumber.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class RotateNumber{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("enter the number: ");
+        int num = scn.nextInt();
+
+        System.out.println("enter the key: ");
+        int key = scn.nextInt();
+
+        int place = 0;
+        int tempNum = num;
+        while(tempNum > 0){
+            place++;
+            tempNum /= 10;
+        }
+
+        if(key >= 0){
+            while(key > 0){
+                int rem = num % 10;
+                int numByTen = num / 10;
+    
+                num = numByTen + (rem * (int)Math.pow(10, place-1 ));  
+    
+                key--;
+            }
+    
+            System.out.println("rotated number is: " + num);
+        }
+        
+
+        scn.close();
+    }
+}
