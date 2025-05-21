8717183e4ea48523715c08a65f4d0b9151f5d8b1
ashish kumar singh <ashish159singh@gmail.com>
2025-05-21 15:33:28 +0530

    bar chart added

---
+++ JAVA_DSA/array/indexOfElement.java
@@ -0,0 +1,28 @@
+package array;
+
+import java.util.Scanner;
+
+public class indexOfElement {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.print("enter the length of the array: ");
+        int size = scn.nextInt();
+        System.out.print("enter the key: ");
+        int key = scn.nextInt();
+        int arr[] = new int[size];
+
+        for(int i = 0; i < size; i++){
+            System.out.print("enter the value in array [" + i + "]: ");
+            arr[i] = scn.nextInt();
+        }
+
+        int index = -1;
+        for(int i = 0; i < size; i++){
+            if(key == arr[i]){
+               index = i;
+                break;
+            }
+        }
+        System.out.print("\nthe index of key in the array is: "+ index);
+    }
+}
