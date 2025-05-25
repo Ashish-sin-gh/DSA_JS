abeec01e6fb72ac53e2cec1863f46bf578a18f93
ashish kumar singh <ashish159singh@gmail.com>
2025-05-25 11:12:09 +0530

    oops concept update

---
+++ JAVA_DSA/oops/swapMethod3.java
@@ -0,0 +1,46 @@
+package oops;
+
+
+
+public class swapMethod3 {
+    
+    public  static  class Person{
+        int age;
+        String name;
+
+        void getDetails(){
+            System.out.println(age);
+            System.out.println(name);
+        }
+    }
+
+    public static void swap(Person person1, Person person2){
+        int age = person1.age;
+        person1.age = person2.age;
+        person2.age = age;
+
+        String name = person1.name;
+        person1.name = person2.name;
+        person2.name = name;
+    }
+
+
+    public static void main(String[] args){
+        Person p1 = new Person();
+        Person p2 = new Person();
+
+        p1.age = 20;
+        p1.name = "ashish";
+
+        p2.age = 10;
+        p2.name = "hunny";
+
+        p1.getDetails();
+        p2.getDetails();
+
+        swap(p1, p2);
+
+        p1.getDetails();
+        p2.getDetails();
+    }
+}
