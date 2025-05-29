e36f53aa1aaa3014b19971971e3ce166ccdca7aa
ashish kumar singh <ashish159singh@gmail.com>
2025-05-29 14:24:30 +0530

    added member modifer ex

---
+++ JAVA_DSA/oops/memberModifer/A2.java
@@ -0,0 +1,10 @@
+package oops.memberModifer2;
+
+import oops.memberModifer.A1;
+
+public class A2 {
+    public static void main(String[] args) {
+        A1 a = new A1();
+        a.m1();
+    }
+}
