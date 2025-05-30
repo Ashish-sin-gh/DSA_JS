27426f792eb3968908e1eed71d43d48c8f759457
ashish kumar singh <ashish159singh@gmail.com>
2025-05-30 14:10:07 +0530

    member modifer protected added

--- JAVA_DSA/oops/memberModifer/A2.java
+++ JAVA_DSA/oops/memberModifer/A2.java
@@ -3,9 +3,15 @@ package oops.memberModifer2;
 import oops.memberModifer.A1;
 
 public class A2 {
+
+    private static void m3(){
+        System.out.println("I was called within a the class where are i was declared");
+    }
+
     public static void main(String[] args) {
         A1 a = new A1();
         a.m1();
-        a.m2();
+        // a.m2();
+        m3();
     }
 }
