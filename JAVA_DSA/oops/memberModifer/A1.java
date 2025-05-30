d9284d286bc540c1d5ffd9b92a9824b135e6209e
ashish kumar singh <ashish159singh@gmail.com>
2025-05-30 12:00:20 +0530

    member modifer updated

--- JAVA_DSA/oops/memberModifer/A1.java
+++ JAVA_DSA/oops/memberModifer/A1.java
@@ -1,7 +1,14 @@
 package oops.memberModifer;
 
 public class A1 {
+    // public member -> can be accessed from anywhere provided its class is public
     public void m1(){
-        System.out.println("in A method");
+        System.out.println("in public method");
     }    
+
+    // default member -> can be accessed only within the package 
+        void m2(){
+            System.out.println("\nthis is a default method");
+            System.out.println("this op ensures that method is default memeber and is being called from same package");
+        }  
 }
--- JAVA_DSA/oops/memberModifer/A2.java
+++ JAVA_DSA/oops/memberModifer/A2.java
@@ -6,5 +6,6 @@ public class A2 {
     public static void main(String[] args) {
         A1 a = new A1();
         a.m1();
+        a.m2();
     }
 }
