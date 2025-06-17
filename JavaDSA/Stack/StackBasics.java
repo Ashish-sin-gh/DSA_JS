b7e973bee68c01cec282d8219b0ea217e6fc6b21
ashish kumar singh <ashish159singh@gmail.com>
2025-06-17 19:37:08 +0530

    stack problem added

--- JavaDSA/Stack/StackBasics.java
+++ JavaDSA/Stack/StackBasics.java
@@ -2,14 +2,30 @@
  * stack is a dataStructure like we have array, ArrayList, String
  * stack is always Last In First Out (LIFO)
  * 
- * operations in stack -
- *      push - "add" to stack
- *      pop - "remove" from stack 
- *      peak - get the "top" element in the stack
+ * operations/ methods in stack -
+ *      push() - "add" to stack
+ *      pop() - "remove" from stack 
+ *      peek() - get the "top" element in the stack
+ *      size() - get the size of the stack
  */
-package java
+package JavaDSA.Stack;
+
+import java.util.*;
+
  public class StackBasics{
     public static void main(String[] args) {
-        
+        Stack<Integer> st; // decaration 
+        st = new Stack<Integer>(); // initilization
+
+        st.push(10);
+        st.push(40);
+        st.push(60);
+        st.push(80);
+        st.push(10);
+        System.out.println(st);
+        st.pop();
+        System.out.println(st);
+        System.out.println(st.peek());
+        System.out.println(st.size());
     }
  }
