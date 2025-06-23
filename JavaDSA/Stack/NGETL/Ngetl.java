edfc995d220bf37a9e04da8a089ca3c54ba3f1ac
ashish kumar singh <ashish159singh@gmail.com>
2025-06-23 19:18:31 +0530

    stack added

---
+++ JavaDSA/Stack/NGETL/Ngetl.java
@@ -0,0 +1,54 @@
+// next greatest number to the left
+
+/*
+ * from start of the array 
+ * 
+ */
+
+package JavaDSA.Stack.NGETL;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Ngetl {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
+        Stack<Integer> st = new Stack<>();
+        System.out.println("enter the size of the array");
+        int size = scn.nextInt();
+        
+        int[] arr = new int[size];
+        
+        System.out.println("enter the value for the array");
+        for(int i = 0 ; i < size; i ++){
+            arr[i] = scn.nextInt();
+        }
+        
+        int[] arrAnswer = new int[arr.length];
+
+        for(int i = 0; i < arr.length; i ++){
+            if(i == 0){
+                st.push(arr[0]);
+                arrAnswer[i] = -1;
+            }
+            while(!st.isEmpty() && arr[i] >= st.peek() ){
+                st.pop();  
+            }
+            
+            if(st.isEmpty()){
+                arrAnswer[i] = -1;
+            } else {
+                arrAnswer[i] = st.peek();
+            }
+
+            st.push(arr[i]);
+        }
+
+        for(int i = 0; i < arrAnswer.length; i++){
+            System.out.println("next greater for " + arr[i] + " is " + arrAnswer[i]);
+        }
+
+        scn.close();
+    }
+}
