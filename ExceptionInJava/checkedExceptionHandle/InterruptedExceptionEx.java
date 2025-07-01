a8c458e28685d858237bf6087fa7168aec958c46
ashish kumar singh <ashish159singh@gmail.com>
2025-07-01 15:52:54 +0530

    checked exception handled

---
+++ ExceptionInJava/checkedExceptionHandle/InterruptedExceptionEx.java
@@ -0,0 +1,8 @@
+package ExceptionInJava.checkedExceptionHandle;
+
+public class InterruptedExceptionEx {
+    public static void main(String[] args){
+        Thread.sleep(1000); 
+        System.out.println("hello wold");
+    }
+}
