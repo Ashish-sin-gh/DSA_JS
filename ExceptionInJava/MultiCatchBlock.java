849daf9adf2763d05c1adda660eac373cf373d71
ashish kumar singh <ashish159singh@gmail.com>
2025-07-08 14:57:33 +0530

    final commit for exception

--- ExceptionInJava/MultiCatchBlock.java
+++ ExceptionInJava/MultiCatchBlock.java
@@ -1,5 +1,8 @@
 package ExceptionInJava;
 
+import java.io.IOError;
+import java.io.IOException;
+
 public class MultiCatchBlock{
     public static void main(String[] args){
         try{
@@ -8,6 +11,13 @@ public class MultiCatchBlock{
             System.out.println(str.length());
         } catch(NullPointerException | ArithmeticException e){
             e.printStackTrace();
+        } 
+        /*
+        catch(IOException | Exception e){
+            e.printStackTrace();
         }
+
+        there should be no relation btn exception in same catch block
+        */
     }
 }
