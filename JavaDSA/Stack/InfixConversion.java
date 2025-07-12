aaaaef2ea827cf00a9c3608208935c27eb6a27a6
ashish kumar singh <ashish159singh@gmail.com>
2025-07-12 12:53:43 +0530

    stack and queue extended

---
+++ JavaDSA/Stack/InfixConversion.java
@@ -0,0 +1,104 @@
+// convert infix expression to postfix and prefix expression
+
+/*
+ * infix - what human understand -> a + b - ( c * d ) / e + f;
+ * prefix - what machine understand -> + + a b - / * c d e f
+ * postfix - what machine understand -> a b + c d * e / - f +
+*/
+package JavaDSA.Stack;
+
+import java.io.BufferedReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class InfixConversion{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("enter the infix Expression: ");
+        String infixExp = br.readLine();
+
+        Stack<String> prefixStack = new Stack<>();
+        Stack<String> postfixStack = new Stack<>();
+        Stack<Character> operatorStack = new Stack<>();
+
+        for(int i = 0; i < infixExp.length(); i ++){
+            char ch = infixExp.charAt(i);
+
+            if(ch == '('){
+                operatorStack.push(ch);
+
+            } else if(Character.isDigit(ch)){
+                prefixStack.push(String.valueOf(ch));
+                postfixStack.push(String.valueOf(ch));
+                
+            } else if(ch == ')'){
+                while(!operatorStack.isEmpty() && operatorStack.peek() != '('){
+                    char operator = operatorStack.pop();
+
+                    // prefix
+                    String operand1OfPrefix = prefixStack.pop();
+                    String operand2OfPrefix = prefixStack.pop();
+                    String result1 = operator + operand2OfPrefix + operand1OfPrefix;
+                    prefixStack.push(result1);
+
+                    // postfix
+                    String operand1OfPostfix = postfixStack.pop();
+                    String operand2OfPostfix = postfixStack.pop();
+                    String result2 = operand2OfPostfix + operand1OfPostfix + operator;  
+                    postfixStack.push(result2);
+                }
+                operatorStack.pop();
+
+            } else{
+                while(!operatorStack.isEmpty() && operatorStack.peek() != '(' && precedence(ch) <= precedence(operatorStack.peek())){
+                    char operator = operatorStack.pop();
+
+                    // prefix
+                    String operand1OfPrefix = prefixStack.pop();
+                    String operand2OfPrefix = prefixStack.pop();
+                    String result1 = operator + operand2OfPrefix + operand1OfPrefix;
+                    prefixStack.push(result1);
+
+                    // postfix
+                    String operand1OfPostfix = postfixStack.pop();
+                    String operand2OfPostfix = postfixStack.pop();
+                    String result2 = operand2OfPostfix + operand1OfPostfix + operator; 
+                    postfixStack.push(result2);
+                }
+                operatorStack.push(ch);
+            }
+        }
+
+        while(!operatorStack.isEmpty()){
+            char operator = operatorStack.pop();
+
+                    // prefix
+                    String operand1OfPrefix = prefixStack.pop();
+                    String operand2OfPrefix = prefixStack.pop();
+                    String result1 = operator + operand2OfPrefix + operand1OfPrefix;
+                    prefixStack.push(result1);
+
+                    // postfix
+                    String operand1OfPostfix = postfixStack.pop();
+                    String operand2OfPostfix = postfixStack.pop();
+                    String result2 = operand2OfPostfix + operand1OfPostfix + operator; 
+                    postfixStack.push(result2);
+        }
+
+        System.out.println("converted prefix exp: " + prefixStack.peek() );
+        System.out.println("converted postfix exp: " + postfixStack.peek() );
+    }
+
+    public static int precedence(char ch) {
+        return switch(ch){
+            case '+' -> 1;
+            case '-' -> 1;
+            case '*' -> 2;
+            default -> 2; 
+        };
+    }
+
+}
+
