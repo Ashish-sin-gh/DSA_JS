d71b2febb94c67cfb88a7a4bb4e7850552e1f9b5
ashish kumar singh <ashish159singh@gmail.com>
2025-07-31 14:32:52 +0530

    static length queue

--- DataStuctureImplemation/Stack/QueueImplementation.java
+++ DataStuctureImplemation/Stack/QueueImplementation.java
@@ -2,42 +2,45 @@ package DataStuctureImplemation.Stack;
 
 public class QueueImplementation {
     public static void main(String[] args){
-        CustomQueue queue = new CustomQueue(10);
+        CustomQueue queue = new CustomQueue(3);
         queue.push(10);
         queue.push(20);
         queue.push(30);
-        queue.push(40);
-        queue.pop();
-        queue.pop();
-        System.out.println(queue.peek());
+        System.out.println(queue.pop() + " poped");
+        System.out.println(queue.pop() + " poped");
+        // System.out.println(queue.pop() + " poped");
+        queue.push(30);
+        System.out.println("peek: " + queue.peek());
     }
 
     public static class CustomQueue{
         private int[] queueArray;
-        private int startpointer;
+        private int startPointer;
         private int endPointer;
         private int length;
+        private int currentSize;
 
         CustomQueue(int length){
             this.length = length;
             this.endPointer = -1;
-            this.startpointer = 0;
+            this.startPointer = -1;
+            this.currentSize = 0;
             
             this.queueArray = new int[this.length];
         }
 
         // is full ?
         public boolean isFull(){
-            if(this.length != this.endPointer){
-                return false;
-            } else {
+            if(this.currentSize == this.length){
                 return true;
+            } else {
+                return false;
             }
         }
 
         // is empty ?
         public boolean isEmpty(){
-            if(this.endPointer == -1){
+            if(this.currentSize == 0){
                 return true;
             } else {
                 return false;
@@ -47,31 +50,36 @@ public class QueueImplementation {
         // push element
         public void push(int number){
             if(!this.isFull()){
-                this.endPointer ++;
+                this.currentSize ++;
+                this.endPointer = (this.endPointer + 1) % this.length;
                 this.queueArray[this.endPointer] = number;
             } else {
-                System.out.println("arry is full: cant push " + number);
+                System.err.println("cant push any new number as queue is empty");
                 return;
             }
         }
 
         public int pop(){
             if(!this.isEmpty()){
-                this.startpointer ++;
-                return this.queueArray[this.startpointer];
+                if(this.currentSize == 1){
+                    this.currentSize = 0;
+                    this.startPointer = -1;
+                    this.endPointer = -1;
+                } else {
+                    this.currentSize --;
+                }
+                this.startPointer = (this.startPointer + 1) % this.length;
+                return this.queueArray[this.startPointer];
             } else {
-                System.out.println("Stack is empty: cant pop anything");
                 return -1;
             }
         }
 
         public int peek(){
-            return this.queueArray[this.startpointer]; 
-        }
-
-        public void display(){
-            for(int i = this.startpointer; i <= this.endPointer; i++){
-                System.out.println(this.queueArray[i]);
+            if(!isEmpty()){
+                return this.queueArray[this.startPointer];
+            } else{
+                return -1;
             }
         }
     }
