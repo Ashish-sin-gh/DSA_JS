af986f4430e3a7c208205d50d094f90d25c9c583
ashish kumar singh <ashish159singh@gmail.com>
2025-08-05 18:03:02 +0530

    linkedlist operation- delete incomplete code

--- JavaDSA/linkedList/DeletionInLinkedList/Delete1stElement.java
+++ JavaDSA/linkedList/DeletionInLinkedList/Delete1stElement.java
@@ -2,6 +2,30 @@ package linkedList.DeletionInLinkedList;
 
 public class Delete1stElement {
     public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50,60,70,80,90,100};
         
+        Node head = new Node(arr[0]);
+        Node moveablePointer = head;
+
+        for(int i = 1; i < arr.length; i++){
+            Node unlinkedNextNode = new Node(arr[i]);
+            moveablePointer.nextNode = unlinkedNextNode;
+            moveablePointer = moveablePointer.nextNode; 
+        }
+
     }
+
+    public static class Node{
+        int data; 
+        Node nextNode;
+
+        Node(int element, Node referenceNextNode){
+            this.data = element;
+            this.nextNode = referenceNextNode;
+        }
+
+        Node(int element){
+            this.data = element;
+            this.nextNode = null;
+        }
 }
